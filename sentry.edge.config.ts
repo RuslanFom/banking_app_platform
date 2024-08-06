26fd13bd7e917e25cdbae4df3e2bea8b17e21339
RuslanFom <ruslanfom@gmail.com>
2024-08-06 12:28:13 +0200

    install Sentry for security and fast fix of bank app

---
+++ sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://b48ccc920b73db615b4fcc1fe2528a68@o4507729899814912.ingest.de.sentry.io/4507729903878224",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
