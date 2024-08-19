763ee7f58edc91d064138de57c9448ac427dc26f
RuslanFom <ruslanfom@gmail.com>
2024-08-19 12:51:26 +0200

    implement new bankTransactons components

---
+++ components/RecentTransactions.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import Link from "next/link";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {BankTabItem} from "@/components/BankTabItem";
+import BankInfo from "@/components/BankInfo";
+import TransactionsTable from "@/components/TransactionsTable";
+
+
+
+const RecentTransactions = ({
+                                accounts,
+                                transactions = [],
+                                appwriteItemId,
+                                page = 1,
+                            }: RecentTransactionsProps) => {
+    return (
+        <section className='recent-transactions'>
+            <header className='flex items-center justify-between'>
+                <h2 className='recent-transactions-label'>
+                    recent transactions
+                </h2>
+                <Link
+                    href={`/transactions-history/?id=${appwriteItemId}`}
+                    className='view-all-btn'
+                >
+                    View all transactions
+                </Link>
+            </header>
+
+            <Tabs defaultValue={appwriteItemId} className="w-full">
+                <TabsList className="recent-transactions-tablist">
+                    {accounts?.map((account: Account) => (
+                        <TabsTrigger key={account.id} value={account.appwriteItemId}>
+                            <BankTabItem
+                                key={account.id}
+                                account={account}
+                                appwriteItemId={appwriteItemId}
+                            />
+                        </TabsTrigger>
+                    ))}
+                </TabsList>
+
+                {accounts.map((account: Account) => (
+                    <TabsContent
+                        className="space-y-4"
+                        key={account.id}
+                        value={account.appwriteItemId}
+                    >
+                        <BankInfo
+                            account={account}
+                            appwriteItemId={appwriteItemId}
+                            type='full'
+                        />
+                        <TransactionsTable
+                            transactions={transactions}
+                        />
+                    </TabsContent>
+                ))}
+
+            </Tabs>
+
+        </section>
+    )
+}
+
+export default RecentTransactions;
